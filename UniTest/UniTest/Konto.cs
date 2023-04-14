834d3e687477944bb93b9ad64ba3d4bb924bb463
FalkTreudt <98171639+FalkTreudt@users.noreply.github.com>
2023-04-14 11:44:32 +0200

    PPAufgabe2

---
+++ UniTest/UniTest/Konto.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+class Konto
+{
+    static void Main()
+    {
+        Konto konto1 = new Konto(1, 1000);
+        Konto konto2 = new Konto(2, 1500);
+        Konto konto3 = new Konto(3, 1700);
+
+        konto1.Auszahlen(500);
+        konto2.Einzahlen(10);
+        konto3.Auszahlen(222);
+
+        Console.WriteLine($"Kontonummer1: {konto1.Nummer} Betrag1: {konto1.Betrag}");
+        Console.WriteLine($"Kontonummer2: {konto2.Nummer} Betrag2: {konto2.Betrag}");
+        Console.WriteLine($"Kontonummer3: {konto3.Nummer} Betrag3: {konto3.Betrag}");
+
+        Console.ReadKey();
+
+    }
+    public int Nummer { get; private set; }
+    public float Betrag { get; private set; }
+
+    public Konto(int nummer, float betrag)
+    {
+        Nummer = nummer;
+        Betrag = betrag;
+    }
+
+    public void Einzahlen(float wert)
+    {
+        if (wert < 0)
+        {
+            Console.WriteLine("Fehler: Einzahlungsbetrag muss positiv sein.");
+        }
+        else
+        {
+            Betrag += wert;
+        }
+    }
+
+    public void Auszahlen(float wert)
+    {
+        if (wert < 0)
+        {
+            Console.WriteLine("Fehler: Auszahlungsbetrag muss positiv sein.");
+        }
+        else if (wert > Betrag)
+        {
+            Console.WriteLine("Fehler: Nicht genug Geld auf dem Konto.");
+        }
+        else
+        {
+            Betrag -= wert;
+        }
+    }
+}
---
+++ UniTest/UniTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n1 = 0, n2 = 1, n3, number = 55;
+
+        Console.Write(n1 + " " + n2 + " ");
+
+        while (n1 + n2 <= number)
+        {
+            n3 = n1 + n2;
+            Console.Write(n3 + " ");
+            n1 = n2;
+            n2 = n3;
+        }
+
+        Console.ReadKey();
+    }
+}
